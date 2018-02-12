271d50bd32fe5851eb237d5eeeaf22763cb078d9
Óscar Ferrándiz <osalarcon@homeaway.com>
2018-02-12 16:07:33 +0100

    Initial commit

---
+++ src/server/server.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Hapi from 'hapi';
+import ReactDOMServer from 'react-dom/server';
+import App from '../universal/components/App';
+
+const server = Hapi.server({
+  host: 'localhost',
+  port: 8000,
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  config: {
+    state: {
+      parse: false,
+      failAction: 'ignore',
+    },
+  },
+  handler() {
+    return `<html><head></head><body>${ReactDOMServer.renderToString(<App />)}</body></html>`;
+  },
+});
+
+async function start() {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log('Server running at:', server.info.uri);
+}
+
+start();
